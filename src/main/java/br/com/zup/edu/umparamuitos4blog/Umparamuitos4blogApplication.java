1b3256f5ef63c2d8b7bf9a3545d4acf542ca534c
Denes <jose.araujo@zup.com.br>
2022-04-07 17:08:42 -0300

    criando a aplicacao

---
+++ src/main/java/br/com/zup/edu/umparamuitos4blog/Umparamuitos4blogApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.umparamuitos4blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umparamuitos4blogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umparamuitos4blogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/umparamuitos4blog/Umparamuitos4blogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.umparamuitos4blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umparamuitos4blogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
